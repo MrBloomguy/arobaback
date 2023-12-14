5818a4c8653281b227b1349c7f988f9814a89d8c
0xmikki <90579247+MrBloomguy@users.noreply.github.com>
2023-12-14 18:51:43 +0100

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/gmits-riddhijoshi/flutter_sdks/flutter_3_16_0"
+export "FLUTTER_APPLICATION_PATH=/home/gmits-riddhijoshi/dev/live/socialv-flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=6.10.2"
+export "FLUTTER_BUILD_NUMBER=6.10.2"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
